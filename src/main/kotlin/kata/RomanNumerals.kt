509c7086764c8cb39e225f7bdc432d549180b0b9
David Cortes Cutillas <dcortes@Davids-MacBook-Air.local>
2024-07-19 16:09:04 +0200

    Version of roman numerals convert from number

--- src/main/kotlin/kata/RomanNumerals.kt
+++ src/main/kotlin/kata/RomanNumerals.kt
@@ -4,9 +4,17 @@ class RomanNumerals {
 
     companion object {
 
-        fun convert(number: Int): String {
-           TODO()
-        }
+        private val numbers =
+            listOf(50 to "L", 49 to "IL", 40 to "XL", 10 to "X", 9 to "IX", 5 to "V", 4 to "IV", 1 to "I")
+
+        fun convert(number: Int): String =
+            if (number > 0) {
+                val (num, roman) = numbers.find { it.first <= number }!!
+                roman + convert(number - num)
+            } else {
+                ""
+            }
+
 
         fun convert(romanNumeral: String): Int {
             TODO()
