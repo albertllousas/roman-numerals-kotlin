95aecbc0b58ca2e8bf6a42a0be9af3891cbfcfb8
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-07-19 15:30:33 +0200

    restart

--- src/main/kotlin/kata/RomanNumerals.kt
+++ src/main/kotlin/kata/RomanNumerals.kt
@@ -5,39 +5,11 @@ class RomanNumerals {
     companion object {
 
         fun convert(number: Int): String {
-            return when (number) {
-                0 -> ""
-                1 -> "I"
-                2 -> "I" + convert(number - 1)
-                3 -> "I" + convert(number - 1)
-                4 -> "IV"
-                5 -> "V"
-                6 -> "V" + convert(number - 5)
-                7 -> "V" + convert(number - 5)
-                8 -> "V" + convert(number - 5)
-                9 -> "IX"
-                10 -> "X"
-                11 -> "X" + convert(number - 10)
-                12 -> "X" + convert(number - 10)
-                13 -> "X" + convert(number - 10)
-                14 -> "X" + convert(number - 10)
-                15 -> "X" + convert(number - 10)
-                16 -> "X" + convert(number - 10)
-                17 -> "X" + convert(number - 10)
-                18 -> "X" + convert(number - 10)
-                19 -> "X" + convert(number - 10)
-                20 -> "X" + convert(number - 10)
-                21 -> "X" + convert(number - 10)
-                22 -> "X" + convert(number - 10)
-                23 -> "X" + convert(number - 10)
-                24 -> "X" + convert(number - 10)
-                25 -> "X" + convert(number - 10)
-                26 -> "X" + convert(number - 10)
-                in 27 .. 38 -> "X" + convert(number - 10)
-                39 -> "XXXIX"
-                in 40 .. 48 -> "XL" + convert(number - 40)
-                else -> "IL"
-            }
+           TODO()
+        }
+
+        fun convert(romanNumeral: String): Int {
+            TODO()
         }
     }
 }
--- src/test/kotlin/kata/RomanNumeralsTest.kt
+++ src/test/kotlin/kata/RomanNumeralsTest.kt
@@ -1,6 +1,7 @@
 package kata
 
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.TestFactory
 
@@ -10,28 +11,59 @@ class RomanNumeralsTest {
     fun `should convert decimals to roman numerals`() =
         listOf(
             Pair(1, "I"),
-            Pair(2, "II"),
-            Pair(3, "III"),
-            Pair(4, "IV"),
-            Pair(5, "V"),
-            Pair(6, "VI"),
-            Pair(7, "VII"),
-            Pair(8, "VIII"),
-            Pair(9, "IX"),
-            Pair(10, "X"),
-            Pair(11, "XI"),
-            Pair(14, "XIV"),
-            Pair(15, "XV"),
-            Pair(19, "XIX"),
-            Pair(20, "XX"),
-            Pair(37, "XXXVII"),
-            Pair(39, "XXXIX"),
-            Pair(40, "XL"),
-            Pair(41, "XLI"),
-            Pair(49, "IL"),
+//            Pair(2, "II"),
+//            Pair(3, "III"),
+//            Pair(4, "IV"),
+//            Pair(5, "V"),
+//            Pair(6, "VI"),
+//            Pair(7, "VII"),
+//            Pair(8, "VIII"),
+//            Pair(9, "IX"),
+//            Pair(10, "X"),
+//            Pair(11, "XI"),
+//            Pair(14, "XIV"),
+//            Pair(15, "XV"),
+//            Pair(19, "XIX"),
+//            Pair(20, "XX"),
+//            Pair(37, "XXXVII"),
+//            Pair(39, "XXXIX"),
+//            Pair(40, "XL"),
+//            Pair(41, "XLI"),
+//            Pair(49, "IL"),
         ).map { (decimal, expected) ->
             dynamicTest("$decimal $expected") {
                 RomanNumerals.convert(decimal) shouldBe expected
+            }
+        }
+
+
+    @Disabled
+    @TestFactory
+    fun `should convert roman numerals to decimals`() =
+        listOf(
+            Pair("I", 1),
+//            Pair("II", 2),
+//            Pair("III", 3),
+//            Pair("IV", 4),
+//            Pair("V", 5),
+//            Pair("VI", 6),
+//            Pair("VII", 7),
+//            Pair("VIII", 8),
+//            Pair("IX", 9),
+//            Pair( "X", 10),
+//            Pair( "XI", 11),
+//            Pair( "XIV", 14),
+//            Pair( "XV", 15),
+//            Pair( "XIX", 19),
+//            Pair( "XX", 20),
+//            Pair( "XXXVII", 37),
+//            Pair( "XXXIX", 39),
+//            Pair( "XL", 40),
+//            Pair( "XLI", 41),
+//            Pair( "IL", 49),
+        ).map { (romanNumeral, expected) ->
+            dynamicTest("$romanNumeral $expected") {
+                RomanNumerals.convert(romanNumeral) shouldBe expected
             }
         }
 }
